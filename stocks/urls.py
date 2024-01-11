f23dc258bbe90e0ac3bd006df6f090138f2f915c
AirFryedCoffee <jpbryant6@gmail.com>
2024-01-10 22:56:46 -0800

    Image Recognition Changes
    Image Recognition Changes, change the background of all pages tomorrow or this weekend

--- stocks/urls.py
+++ stocks/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -6,5 +8,4 @@ urlpatterns = [
     path("prices/", views.get_price, name="get_price"),
     path("home/", views.trending_tickers_view, name="trending_tickers"),
     path("", views.new_user_stock, name="new_stock"),
-]
-
+] 
